632f61892627e4044fc5e77b31ccfd0b3faffbd8
willmana <aleksi.willman@gmail.com>
2021-02-05 18:15:35 +0200

    asdfasdf

---
+++ test/src/App.js
@@ -0,0 +1,21 @@
+
+import './App.css';
+
+
+const App = () => {
+  console.log("asdfasdfasdfasfda")
+  
+  return (
+    <div>
+      <h1>
+        Hello
+      </h1>
+      <h2>
+        bitch
+      </h2>
+    </div>
+  )
+
+}
+
+export default App;
